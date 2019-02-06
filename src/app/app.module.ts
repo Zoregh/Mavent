1a60531492114e3dee3cb91fa29f3ea2dc797a6f
Zoregh <zoreghhakobyan@gmail.com>
2019-02-07 00:44:16 +0400

    added service & components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { ItemsComponent } from './Components/items/items.component';
+import { ItemComponent } from './Components/item/item.component';
+import { ObjectComponent } from './Components/object/object.component';
+import { ObjectsComponent } from './Components/objects/objects.component';
+import { NewsComponent } from './Components/news/news.component';
+import { AllnewsComponent } from './Components/allnews/allnews.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ItemsComponent,
+    ItemComponent,
+    ObjectComponent,
+    ObjectsComponent,
+    NewsComponent,
+    AllnewsComponent
   ],
   imports: [
     BrowserModule,
