84b9ce0d6839b1c9c53eea17b1e7c2c16dae61fc
Zoregh <zoreghhakobyan@gmail.com>
2019-02-12 20:21:57 +0400

    changing language via local storage

--- src/app/Services/general.service.ts
+++ src/app/Services/general.service.ts
@@ -5,16 +5,30 @@ import { HttpClient }    from '@angular/common/http';
   providedIn: 'root'
 })
 export class GeneralService {
+  public lng: string = 'ru';
   public sliders: any = [];
   public products: Array<any> = [];
 
   constructor(private http: HttpClient) { 
+    this.checkLang();
+  }
+
+  checkLang(){
+    const storedLang = JSON.parse(localStorage.getItem('Language'));
+    if (storedLang) {
+      this.lng = storedLang;
+    }
     this.getSlider1();
     this.getProducts();
   }
 
+  setLang(a){
+    localStorage.setItem('Language', JSON.stringify(a));
+    this.checkLang();
+  }
+
   getSlider1(){
-    this.http.get('http://alikogrd.beget.tech/api/slide/ru/slides').subscribe(
+    this.http.get('http://alikogrd.beget.tech/api/slide/' + this.lng + '/slides').subscribe(
       (data: any) => {
         this.sliders = data.slice();
       },
@@ -25,10 +39,9 @@ export class GeneralService {
   }
 
   getProducts(){
-    this.http.get('http://alikogrd.beget.tech/api/product/ru/type-products?limit=5').subscribe(
+    this.http.get('http://alikogrd.beget.tech/api/product/' + this.lng + '/type-products?limit=5').subscribe(
       (data: any) => {
-        console.log(data);
-        
+        this.products = [];
         for (let i = 0; i < data.length; i++) {
           this.products = this.products.concat(data[i].products);
         }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GeneralService } from './Services/general.service'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private serve: GeneralService) { }
   title = 'Mavent';
 }
